7e18968c64cd2a103a4fbfced3101de5160007b6
Арман <@ARMAN>
2023-11-12 16:09:08 +0600

    Добавьте файлы проекта.

---
+++ prac10(2)/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+// Определение интерфейса IStudent
+public interface IStudent
+{
+    string Name { get; set; }
+    string FullName { get; set; }
+    int[] Grades { get; set; }
+
+    string GetName();
+    string GetFullName();
+    double GetAvgGrade();
+}
+
+// Реализация интерфейса IStudent в классе Student
+public class Student : IStudent
+{
+    public string Name { get; set; }
+    public string FullName { get; set; }
+    public int[] Grades { get; set; }
+
+    public Student(string name, string fullName, int[] grades)
+    {
+        Name = name;
+        FullName = fullName;
+        Grades = grades;
+    }
+
+    public string GetName()
+    {
+        return Name;
+    }
+
+    public string GetFullName()
+    {
+        return FullName;
+    }
+
+    public double GetAvgGrade()
+    {
+        if (Grades.Length == 0)
+        {
+            return 0.0;
+        }
+
+        int sum = 0;
+        foreach (int grade in Grades)
+        {
+            sum += grade;
+        }
+
+        return (double)sum / Grades.Length;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Создание объекта Student и использование методов интерфейса
+        IStudent student = new Student("John", "John Smith", new int[] { 90, 85, 92, 78, 88 });
+        Console.WriteLine("Name: " + student.GetName());
+        Console.WriteLine("Full Name: " + student.GetFullName());
+        Console.WriteLine("Average Grade: " + student.GetAvgGrade());
+    }
+}
---
+++ prac10(2)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("prac10(2)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("prac10(2)")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1d13e0cc-ccc3-46fd-ac47-4a6867b1db92")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
